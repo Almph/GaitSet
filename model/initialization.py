6da43becedf9ddec042daa8ad2f9d82cbb113f6c
pyhe <pyhe19@fudan.edu.cn>
2020-11-06 17:30:32 +0800

    为initialization.py添加注释

--- model/initialization.py
+++ model/initialization.py
@@ -12,16 +12,21 @@ from .model import Model
 
 def initialize_data(config, train=False, test=False):
     #这里传进来的train=True。
+    #
     print("Initializing data source...")
     train_source, test_source = load_data(**config['data'], cache=(train or test))
     #双**星号表示把字典解包为key=value的形式传参给函数，要求key和函数的形参名能对应起来。
     #这里cache必然是True，就是数据的缓存不管是训练还是测试都会做。
+    #这里的cache是告诉后续的data_loader，数据已经缓存好存在DataSet（.data属性）里面了。
     if train:
         print("Loading training data...")
         train_source.load_all_data()
+        #加载数据缓存好，这里其实与cache参数无关，不管真假都会把所有数据都加载一遍。
+
     if test:
         print("Loading test data...")
         test_source.load_all_data()
+    
     print("Data initialization complete.")
     return train_source, test_source
 
@@ -31,10 +36,17 @@ def initialize_model(config, train_source, test_source):
     data_config = config['data']
     model_config = config['model']
     model_param = deepcopy(model_config)
+    #深拷贝。
+
     model_param['train_source'] = train_source
     model_param['test_source'] = test_source
+    #直接把两个数据集当做模型参数传进去对模型初始化。
     model_param['train_pid_num'] = data_config['pid_num']
+    #这是新加入的一个键值对。
+
     batch_size = int(np.prod(model_config['batch_size']))
+    #用于计算batch_size[0]*batch_size[1]的值，128。
+
     model_param['save_name'] = '_'.join(map(str,[
         model_config['model_name'],
         data_config['dataset'],
@@ -48,8 +60,11 @@ def initialize_model(config, train_source, test_source):
     ]))
 
     m = Model(**model_param)
+    #将设置好的参数传入对模型初始化。
+
     print("Model initialization complete.")
     return m, model_param['save_name']
+    #返回模型和模型名称。
 
 
 def initialization(config, train=False, test=False):
@@ -63,4 +78,5 @@ def initialization(config, train=False, test=False):
     #环境的显卡数量。
     train_source, test_source = initialize_data(config, train, test)
     #data按照train=True进行初始化。
+    #两个数据集的.data方法都存好了所有需要的数据。
     return initialize_model(config, train_source, test_source)
